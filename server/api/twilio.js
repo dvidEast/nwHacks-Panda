4c158c9c0614d52749c6d1d5439d0ecce10ab906
koh3n <kohenlee1234@gmail.com>
2024-01-20 14:06:39 -0800

    set up twilio

--- server/api/twilio.js
+++ server/api/twilio.js
@@ -1,12 +1,16 @@
+require('dotenv').config();
+
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const twNumber = process.env.TWILIO_NUMBER;
-const client = require('twilio')(accountSid, authToken);
+const userNumber = process.env.USER_NUMBER;
 
-client.messages
-  .create({
-     body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
+const twilio = require('twilio');
+const client = twilio(accountSid, authToken);
+
+client.messages.create({
+     body: 'get to work.',
      from: twNumber,
-     to: '+15558675310'
+     to: userNumber 
    })
   .then(message => console.log(message.sid));
