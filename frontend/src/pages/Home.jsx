db2d7a3dea7d71d3a883e65431900ca012638bc4
davidlim <daviddlim11@gmail.com>
2024-01-20 21:26:37 -0800

    edited package json

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,72 +6,20 @@ import {videoStart} from '../motion'
 
 
 export default function Home() {
-
-  // const stopCamera = () => {
-  //     handTrack.stopVideo(video);
-  // };
-  // Navigator.getUserMedia =
-  // Navigator.getUserMedia ||
-  // Navigator.webkitUserMedia ||
-  // Navigator.mozUserMedia ||
-  // Navigator.msUserMedia;
-
-  // const modelParams = {
-  // flipHorizontal: true, // flip e.g for video
-  // imageScaleFactor: 0.7, // reduce input image size for gains in speed.
-  // maxNumBoxes: 20, // maximum number of boxes to detect
-  // iouThreshold: 0.5, // ioU threshold for non-max suppression
-  // scoreThreshold: 0.79, // confidence threshold for predictions.
-  // };
-
-  // const video = document.querySelector("#video");
-  // const audio = document.querySelector("#audio");
-  // const canvas = document.querySelector("canvas");
-  // const context = canvas.getContext("2d");
-
-  // let model;
-
-  // handTrack.startVideo(video).then((status) => {
-  // if (status) {
-  //   navigator.getUserMedia(
-  //     { video: {} },
-  //     (stream) => {
-  //       video.srcObject = stream;
-  //       setInterval(runDetection, 1000);
-  //     },
-  //     (err) => console.log(err)
-  //   );
-  // }
-  // });
-
-  // function runDetection() {
-  // model.detect(video).then((predictions) => {
-  //   if (predictions.length > 0) {
-  //     console.log(predictions);
-  //     audio.play();
-  //   }
-  // });
-  // }
-
-  // handTrack.load(modelParams).then((lmodel) => {
-  // model = lmodel;
-  // }); 
-
   const video = document.querySelector("#video-feed");
 
-  useEffect(() => {
-    console.log('test')
-    var canvas = document.querySelector("canvas")
-    console.log("test2", canvas)
-    const video = document.querySelector("canvas");
-    videoStart(video)
-  })
+  // useEffect(() => {
+  //   console.log('test')
+  //   var canvas = document.querySelector("canvas")
+  //   console.log("test2", canvas)
+  //   const video = document.querySelector("canvas");
+  //   videoStart(video)
+  // }, [])
 
   return (
     <>
-      
       <canvas id="canvas">
-      Your browser does not support the canvas tag.
+        Your browser does not support the canvas tag.
       </canvas>
       
     </>
