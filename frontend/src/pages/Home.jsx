0d4131accaca0d2a3a732309f5970ba3f2cd98e6
zjoseph03 <zachjoseph@hotmail.com>
2024-01-20 21:23:28 -0800

    idk what i changed

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -63,15 +63,14 @@ export default function Home() {
     console.log('test')
     var canvas = document.querySelector("canvas")
     console.log("test2", canvas)
-    const video = document.querySelector("canvas");
+    const video = document.querySelector("#video-feed");
     videoStart(video)
   })
 
   return (
     <>
-      
+      <video id="video-feed"></video>
       <canvas id="canvas">
-      Your browser does not support the canvas tag.
       </canvas>
       
     </>
